db146cc81cfada070ca617cd9fa4209938a43864
Manuela Ienuso <lunaienuso@libero.it>
2022-05-27 13:03:57 +0200

    Added resource and main comp

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HeaderComp></HeaderComp>
+    <MainComp></MainComp>
+    <FooterComp></FooterComp>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MainComp from './components/MainComp.vue'
+import HeaderComp from './components/HeaderComp.vue'
+import FooterComp from './components/FooterComp.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    MainComp,
+    HeaderComp,
+    FooterComp
+}
 }
 </script>
 
 <style lang="scss">
+@import "./assets/style.scss";
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background: $charade;
+  color: $white;
 }
 </style>
---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="header container-fluid">
+    <img class="logo" src="../assets/img/avada-music-logo.png" alt="logo">
+    <font-awesome-icon icon="fa-solid fa-bars" />
+  </div>
+</template>
+
+<script>
+export default {
+  name:'HeaderComp'
+}
+</script>
+
+<style lang="scss" scoped>
+.header {
+  box-sizing: content-box;
+  height: 115px;
+  overflow: visible;
+  width: 100%;
+  justify-content: space-around;
+  padding: 20px 20px;
+}
+
+
+</style>
